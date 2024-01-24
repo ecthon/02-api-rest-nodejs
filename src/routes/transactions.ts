18018dd8e526f9d10881e5a604b7ec651641c033
ecthon <ecthon@gmail.com>
2024-01-24 11:07:26 -0400

    Implementando rotas - GET/summary Resumo de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -23,6 +23,13 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return { transaction }
   })
 
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+    return { summary }
+  })
+
   // Create transaction
   app.post('/', async (request, replay) => {
     const createTransactionBodySchema = z.object({
